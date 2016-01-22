53c89b8e04a76a46898b25f1592a5d7cfefcbdfd
soreddysirish <ssireeshreddy@gmail.com>
2016-01-22 16:50:51 +0530

    added validation for group message

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,8 +1,11 @@
 class SessionsController < Devise::SessionsController
   def create
+    @new_user=current_user
+    PrivatePub.publish_to "/users/new", user:@new_user
     super
   end
-  
+
+
 
   def destroy
     current_user.update_attributes(status:false)
